e4b2a1bbb8de589b174b4f6cc1c24b1ff41b8f19
Prateek Anand <52813038+grepprateek@users.noreply.github.com>
2026-06-29 12:18:21 +0200

    Fix image link formatting in About.py

--- About.py
+++ About.py
@@ -4,7 +4,7 @@ st.header("DyNOPS: Dynamic Networks of Outpatient Psychotherapy Symptoms")
 
 col1, col2 = st.columns([1, 2])
 with col1:
-    st.image("DYNOPS.png", link="https://www.tu-braunschweig.de/psychologie/psychotherapieambulanz/team/dynops-der-dino")
+    st.image("DyNOPS.png", link="https://www.tu-braunschweig.de/psychologie/psychotherapieambulanz/team/dynops-der-dino")
 with col2:
     st.text("DyNOPS (Dynamic Bayesian Network Modelling of Psychopathology during Outpatient Psychotherapy) is a collaborative research project between the Institute of Psychology and the Institute of Data Science in Biomedicine at TU Braunschweig. The project aims to view psychological disorders as causal interactions between symptoms forming a continuous feedback loop, rather than as observable parts of a latent condition. DyNOPS uses dynamic Bayesian networks to understand how symptoms of depression and anxiety disorders are interconnected during outpatient psychotherapy.", text_alignment="justify")
 col1, col2, col3 = st.columns([1, 1, 1])
